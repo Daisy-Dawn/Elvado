50d42dc5e2847afb5a5a3742f69c6c1838cab4e5
Daisy-Dawn <agboella20@gmail.com>
2025-02-18 20:27:43 +0100

    updates

--- src/components/trade-comps/tradingviewchart/TradingviewChart.tsx
+++ src/components/trade-comps/tradingviewchart/TradingviewChart.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
 import { useEffect, useRef } from 'react'
 import {
     createChart,
     CandlestickSeries,
     ChartOptions,
+    Time,
 } from 'lightweight-charts'
 
 const CandlestickChart = () => {
@@ -34,70 +37,70 @@ const CandlestickChart = () => {
                     high: 10.63,
                     low: 9.49,
                     close: 9.55,
-                    time: 1642427876,
+                    time: 1642427876 as Time,
                 },
                 {
                     open: 9.55,
                     high: 10.3,
                     low: 9.42,
                     close: 9.94,
-                    time: 1642514276,
+                    time: 1642514276 as Time,
                 },
                 {
                     open: 9.94,
                     high: 10.17,
                     low: 9.92,
                     close: 9.78,
-                    time: 1642600676,
+                    time: 1642600676 as Time,
                 },
                 {
                     open: 9.78,
                     high: 10.59,
                     low: 9.18,
                     close: 9.51,
-                    time: 1642687076,
+                    time: 1642687076 as Time,
                 },
                 {
                     open: 9.51,
                     high: 10.46,
                     low: 9.1,
                     close: 10.17,
-                    time: 1642773476,
+                    time: 1642773476 as Time,
                 },
                 {
                     open: 10.17,
                     high: 10.96,
                     low: 10.16,
                     close: 10.47,
-                    time: 1642859876,
+                    time: 1642859876 as Time,
                 },
                 {
                     open: 10.47,
                     high: 11.39,
                     low: 10.4,
                     close: 10.81,
-                    time: 1642946276,
+                    time: 1642946276 as Time,
                 },
                 {
                     open: 10.81,
                     high: 11.6,
                     low: 10.3,
                     close: 10.75,
-                    time: 1643032676,
+                    time: 1643032676 as Time,
                 },
                 {
                     open: 10.75,
                     high: 11.6,
                     low: 10.49,
                     close: 10.93,
-                    time: 1643119076,
+                    time: 1643119076 as Time,
                 },
                 {
                     open: 10.93,
                     high: 11.53,
                     low: 10.76,
                     close: 10.96,
-                    time: 1643205476,
+                    time: 1643205476 as Time,
                 },
             ]
 
